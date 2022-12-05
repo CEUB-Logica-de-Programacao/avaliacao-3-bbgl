2895f8a6c60cac58a99b743fd5d55c449f4a92d4
Bedablin <110946046+Bedablin@users.noreply.github.com>
2022-12-05 08:27:44 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -22,7 +22,12 @@
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
 def q1(heights):
-    # Escreva seu código aqui
+    max = 0
+
+    for i in heights:
+        if i == 180:
+            max += 1
+
     return 0
 
 
